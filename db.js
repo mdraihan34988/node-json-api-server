b663af1ea74f9670851e7182106f7af2c24d7f1e
mdraihan34988 <swammia908@gmail.com>
2022-08-29 21:13:35 +0600

    formatting

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const todos = require("./data/todos");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
+const tags = require("./data/tags");
 
 module.exports = {
   todos,
   videos,
   videos2,
-  transactions,
+  tags,
 }
 
